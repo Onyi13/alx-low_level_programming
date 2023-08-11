b7de09ab05cc9fe65999204d9d0b3775c8784b78
Onyi13 <njokuonyi562@gmail.com>
2023-08-11 09:57:49 -0700

    JSKDF

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ * Description: This program will print
+ * the alphabet in lowercase using only putchar
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar("%C\n");
+return (0);
+}
