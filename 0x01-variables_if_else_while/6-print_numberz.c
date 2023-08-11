99919a57250b8804edeb868c586c6585a57be7cd
Onyi13 <njokuonyi562@gmail.com>
2023-08-11 10:34:27 -0700

    JSDFK

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Prints all single digit numbers of base 10 starting from 0
+* Description: Using only putchar, prints numbers 0 through 9
+* Return: 0
+*/
+int main(void)
+{int i;
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);}
