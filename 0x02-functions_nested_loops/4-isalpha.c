b8a77572c89549e1cac49f9365ad6110e8a27abd
Onyi13 <njokuonyi562@gmail.com>
2023-08-15 12:30:59 -0700

    SDKJH

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
+#include "main.h"
 /**
- * _isalpha - checks if a character is an alphabetic character.
+ * _isalpha - Checks for an alphabetic character.
  * @c: The character to check.
- *
- * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise.
+ * 
+ * Return: 1 if the character is an alphabet, 0 otherwise.
  */
 int _isalpha(int c)
 {if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-return (1);
-else
+{return (1); }
 return (0); }
