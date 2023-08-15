86e8b3f487204ccc85db527323f3b510b3dc8634
Onyi13 <njokuonyi562@gmail.com>
2023-08-15 12:14:31 -0700

    SDKJF

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,6 @@
-#include <stdio.h>
-void _putchar(char c)
-{putchar(c); }
+#include "main.h"
 /**
- * print_alphabet - prints the lowercase alphabet
+ * print_alphabet - prints the alphabet in lowercase.
  */
 void print_alphabet(void)
 {char letter;
@@ -10,12 +8,9 @@ for (letter = 'a'; letter <= 'z'; letter++)
 {_putchar(letter); }
 _putchar('\n'); }
 /**
- * print_alphabet_x10 - prints the lowercase alphabet 10 times
- * Description: This function uses a loop to call the print_alphabet 
- * function 10 times.
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times.
  */
 void print_alphabet_x10(void)
 {int i;
-for (i = 0; i < 10; i++){print_alphabet(); }}
-int main(void)
-{print_alphabet_x10();return (0); }
+for (i = 0; i < 10; i++)
+{print_alphabet(); }}
