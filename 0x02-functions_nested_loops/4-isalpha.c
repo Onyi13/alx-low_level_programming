de6c95ca4d54382e2331f8bfe88876b84fcf1948
Onyi13 <njokuonyi562@gmail.com>
2023-08-15 12:39:58 -0700

    JHDFS

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,6 @@
  * Return: 1 if the character is an alphabet, 0 otherwise.
  */
 int _isalpha(int c)
-{if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-{
+{if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
 return (1); }
 return (0); }
