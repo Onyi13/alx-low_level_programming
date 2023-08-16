8a245bb2ed420117c7116bf3d6d5b898b6bd932b
Onyi13 <njokuonyi562@gmail.com>
2023-08-16 07:56:41 -0700

    SHKDF

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,8 +8,8 @@
  * Description: The function assumes that the buffer r has enough space.
  */
 void add_strings(char n1[309], char n2[309], char r[309])
-{int carry = 0;
-for (int i = 308; i >= 0; i--)
+{int i, carry = 0;
+for (i = 308; i >= 0; i--)
 {int sum = (n1[i] - '0') + (n2[i] - '0') + carry;
 r[i] = (sum % 10) + '0';
 carry = sum / 10; }}
@@ -21,12 +21,13 @@ carry = sum / 10; }}
 int main(void)
 {char a[309] = "0", b[309] = "1";
 char tmp[309];
-for (int i = 0; i < 309; i++)
+int i;
+for (i = 0; i < 309; i++)
 {a[i] = '0';
 b[i] = '0'; }
 b[308] = '1';
 printf("%s, ", b);
-for (int i = 2; i < 98; i++)
+for (i = 2; i < 98; i++)
 {add_strings(a, b, tmp);
 printf("%s", tmp);
 if (i != 97)
