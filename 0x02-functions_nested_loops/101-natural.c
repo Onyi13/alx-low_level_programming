878d2b231b91caf590e0e16fcccf5bac8cdee5a8
Onyi13 <njokuonyi562@gmail.com>
2023-08-16 00:58:09 -0700

    SJKDF

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,8 +6,8 @@
  * Return: Returns 0 upon successful run.
  */
 int main(void)
-{int i;  // Iterator for our loop
-int sum = 0;  // This will hold the sum of multiples of 3 or 5
+{int i;
+int sum = 0;
 for (i = 1; i < 1024; i++)
 {if (i % 3 == 0 || i % 5 == 0)
 {sum += i; }}
