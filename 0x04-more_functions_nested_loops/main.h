d4caf548ee7c357b9fddac7bd4f598fa4a3139af
Onyi13 <njokuonyi562@gmail.com>
2023-08-17 13:57:04 -0700

    KDSF

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 int _putchar(char c);
 void more_numbers(void);
+void print_line(int n);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
