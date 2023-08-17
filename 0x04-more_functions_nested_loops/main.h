749c57c8691a8016a6c00c1393f6bdb47d3e631c
Onyi13 <njokuonyi562@gmail.com>
2023-08-17 14:17:09 -0700

    JKDFS

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int mul(int a, int b);
 int _putchar(char c);
 void more_numbers(void);
 void print_line(int n);
+void print_square(int size);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
