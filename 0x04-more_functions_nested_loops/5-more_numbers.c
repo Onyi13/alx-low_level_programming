ac5d7341ff79a0ba22400c79da4d7fec74ef4f25
Onyi13 <njokuonyi562@gmail.com>
2023-08-17 13:44:56 -0700

    SDKJ

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * more_numbers - Prints 10 times the numbers, from 0 to 14.
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  */
 void more_numbers(void)
-{int times, num;
-for (times = 0; times < 10; times++)
+{int i, num;
+for (i = 0; i < 10; i++)
 {for (num = 0; num <= 14; num++)
-{if (num / 10)
-_putchar(num / 10 + '0');
+{
+if (num > 9)
+{_putchar(num / 10 + '0'); }
 _putchar(num % 10 + '0'); }
 _putchar('\n'); }}
