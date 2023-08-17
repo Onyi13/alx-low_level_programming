d7d6a0992454d3e464cf42bd99748d6f73281a75
Onyi13 <njokuonyi562@gmail.com>
2023-08-17 14:27:57 -0700

    SDJKFD

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,18 +2,11 @@
 /**
  * main - Entry point of the program. It tests the function print_triangle
  * by printing triangles of various sizes.
- *
  * Return: Always 0 (Success).
  */
 void print_triangle(int size)
-{int i, j;
-if (size <= 0)
-{_putchar('\n');
-return; }
-for (i = 1; i <= size; i++)
-{
-for (j = 1; j <= size - i; j++)
-{_putchar(' ');
-for (j = 1; j <= i; j++)
-{_putchar('#'); }
-_putchar('\n'); }}
+{print_triangle(2);
+print_triangle(10);
+print_triangle(1);
+print_triangle(0);
+return (0); }
