c1c323d4c882bd9f9b742d2f100861f40440fbbc
Onyi13 <njokuonyi562@gmail.com>
2023-08-17 13:28:02 -0700

    SKDH

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,6 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
@@ -12,4 +11,6 @@ int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 int _putchar(char c);
 void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
 #endif /* MAIN_H */
