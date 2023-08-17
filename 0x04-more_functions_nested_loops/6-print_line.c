e7846d4e3a44d999b67b651d9283e19e7a694ce1
Onyi13 <njokuonyi562@gmail.com>
2023-08-17 03:55:41 -0700

    JKSD

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_line - Draws a straight line in the terminal.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{int i;
+for (i = 0; i < n; i++)
+{_putchar('_'); }
+_putchar('\n'); }
