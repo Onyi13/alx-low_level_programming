1f9bd3c267ee4d7af6361929a1ac4f8c8e6301b2
Onyi13 <njokuonyi562@gmail.com>
2023-08-21 11:11:01 -0700

    KJFS

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,8 @@
 /**
  * _puts - Prints a string, followed by a new line, to stdout.
  * @str: The string to be printed.
- * Description: This function takes a string as an argument and iterates through it
+ * Description: This function takes a string as 
+ * an argument and iterates through it
  * to print each character to the standard output, then prints a new line.
  */
 void _puts(char *str)
