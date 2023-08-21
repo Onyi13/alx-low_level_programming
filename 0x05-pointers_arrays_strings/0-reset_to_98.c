87dd763478a7dcf280be8b2ba21663c8ce713f00
Onyi13 <njokuonyi562@gmail.com>
2023-08-21 08:27:57 -0700

    DSKJ

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,10 @@
 #include "main.h"
+/**
+ * reset_to_98 - Updates the value the pointer points to to 98.
+ * @n: A pointer to an integer that we want to update.
+ *
+ * Description: Given a pointer to an int, this function will dereference
+ * the pointer and update the integer it points to, to a value of 98.
+ */
 void reset_to_98(int *n)
 {*n = 98; }
