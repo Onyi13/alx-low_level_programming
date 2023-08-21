2015be3460a5a0b575d75344cdc6b27bf301a18a
Onyi13 <njokuonyi562@gmail.com>
2023-08-21 08:47:21 -0700

    DFSKJ

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,8 @@
 /**
  * _strlen - Returns the length of a string.
  * @s: The string to be measured.
- *
  * Description: Iterates through a string until it reaches a null byte,
  * then returns the number of characters traversed.
- * 
  * Return: The length of the string.
  */
 int _strlen(char *s)
@@ -12,4 +10,4 @@ int _strlen(char *s)
 while (*s)
 {length++;
 s++; }
-return length; }
+return (length); }
