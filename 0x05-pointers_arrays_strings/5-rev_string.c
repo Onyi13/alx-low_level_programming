27e3bb6b8dc645dd2754550c02a00650c3931c1c
Onyi13 <njokuonyi562@gmail.com>
2023-08-21 11:29:27 -0700

    KJSD

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,8 @@
  * rev_string - Reverses a string in place.
  * @s: The string to be reversed.
  * Description: This function takes a string as an argument and reverses it.
- * To do this, it first finds the end of the string and then swaps the characters
+ * To do this, it first finds the
+ * end of the string and then swaps the characters
  * from the beginning and the end moving towards the center.
  */
 void rev_string(char *s)
