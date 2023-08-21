482e0042ef539f721f7473878b44bfe1f602eb58
Onyi13 <njokuonyi562@gmail.com>
2023-08-21 11:12:21 -0700

    KJSDF

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 /**
  * _puts - Prints a string, followed by a new line, to stdout.
  * @str: The string to be printed.
- * Description: This function takes a string as 
+ * Description: This function takes a string as
  * an argument and iterates through it
  * to print each character to the standard output, then prints a new line.
  */
