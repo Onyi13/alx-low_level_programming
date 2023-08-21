3f8b0dbe4970878aa08c6bef2a5aa00a98f133a5
Onyi13 <njokuonyi562@gmail.com>
2023-08-21 08:38:34 -0700

    DSFHK

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,13 @@
-void swap_int(int *a, int *b)
 /**
  * swap_int - Swaps the values of two integers.
  * @a: First integer pointer.
  * @b: Second integer pointer.
- *
- * Description: Takes in two pointers to integers. A temporary variable is used
- * to hold one of the integer's values, allowing the two integers to be swapped.
+ * Description: Takes in two pointers to integers.
+ * A temporary variable is used
+ * to hold one of the integer's values
+ * allowing the two integers to be swapped.
  */
+void swap_int(int *a, int *b)
 {int temp;
 temp = *a;
 *a = *b;
