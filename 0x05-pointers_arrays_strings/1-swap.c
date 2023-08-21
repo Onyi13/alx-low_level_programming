0d71c4ce031381d4aad7aff095206efc54187168
Onyi13 <njokuonyi562@gmail.com>
2023-08-21 08:35:17 -0700

    KSJDF

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,12 @@
 void swap_int(int *a, int *b)
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: First integer pointer.
+ * @b: Second integer pointer.
+ *
+ * Description: Takes in two pointers to integers. A temporary variable is used
+ * to hold one of the integer's values, allowing the two integers to be swapped.
+ */
 {int temp;
 temp = *a;
 *a = *b;
