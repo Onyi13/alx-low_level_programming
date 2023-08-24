25ef4f86b1717bf1a6e1d47b2b5918e862529a38
Onyi13 <njokuonyi562@gmail.com>
2023-08-24 05:57:07 -0700

    DSKJ

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * leet - Encodes a string into 1337.
+ * @s: The string to encode. 
+ * Return: Pointer to the encoded string.
+ */
+char *leet(char *s)
+{int i = 0;
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "4433007711";
+while (s[i])
+{
+for (int j = 0; letters[j]; j++)
+{
+if (s[i] == letter[j]) }
+s[i] = numbers[j];
+break; }}
+i++; }
+return (s); }
