709a6c45df5c0277ad342a070fde2312ba619c34
Onyi13 <njokuonyi562@gmail.com>
2023-08-24 05:34:52 -0700

    DLSK

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * @s: String to be modified. 
+ * Return: Pointer to the modified string.
+ */
+char *string_toupper(char *s)
+{int i = 0;
+while (s[i])
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{s[i] = s[i] - 'a' + 'A'; }
+i++; }
+return s; }
