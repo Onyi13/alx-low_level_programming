b5f4cfa546fe43f1ab7748730961c4098309dc0d
Onyi13 <njokuonyi562@gmail.com>
2023-08-28 08:11:54 -0700

    DSKJ

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ * Return: Nothing.
+ */
 void simple_print_buffer(char *buffer, unsigned int size)
 {unsigned int i;
 i = 0;
@@ -12,6 +18,10 @@ printf("0x%02x", buffer[i]);
 i++; }
 printf("\n"); }
 int main(void)
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
 {char buffer[98] = {0x00};
 simple_print_buffer(buffer, 98);
 _memset(buffer, 0x01, 95);
