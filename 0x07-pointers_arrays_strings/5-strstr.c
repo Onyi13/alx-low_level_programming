dc192681b41a5a1bc7c16ae381a04b67302e580a
Onyi13 <njokuonyi562@gmail.com>
2023-08-28 10:36:44 -0700

    SDJK

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strstr - Locates a substring.
+ * @haystack: String to be searched.
+ * @needle: Substring to be located.
+ * Return: Pointer to the beginning
+ * of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{int i, j;
+if (*needle == '\0')
+return (haystack);
+for (i = 0; haystack[i]; i++)
+{
+for (j = 0; needle[j] && (haystack[i + j] == needle[j]); j++);
+if (needle[j] == '\0')
+return (haystack + i); }
+return (NULL); }
