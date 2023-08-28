30f2b5e88039fbd5a68382963069ce871ce97bef
Onyi13 <njokuonyi562@gmail.com>
2023-08-28 10:37:49 -0700

    SDJHK

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,8 +12,7 @@ char *_strstr(char *haystack, char *needle)
 if (*needle == '\0')
 return (haystack);
 for (i = 0; haystack[i]; i++)
-{
-for (j = 0; needle[j] && (haystack[i + j] == needle[j]); j++);
+{for (j = 0; needle[j] && (haystack[i + j] == needle[j]); j++);
 if (needle[j] == '\0')
 return (haystack + i); }
 return (NULL); }
