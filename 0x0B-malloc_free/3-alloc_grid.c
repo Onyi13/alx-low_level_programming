84c62d4b86c9e452223415dde262de9fdbceb13f
Onyi13 <njokuonyi562@gmail.com>
2023-09-04 12:10:55 -0700

    SKJ

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,8 @@ if (grid == NULL)
 for (i = 0; i < height; i++)
 {grid[i] = (int *)malloc(width * sizeof(int));
 if (grid[i] == NULL)
-{for (j = 0; j < i; j++)
+{
+for (j = 0; j < i; j++)
 {free(grid[j]); }
 free(grid);
 return (NULL); }
