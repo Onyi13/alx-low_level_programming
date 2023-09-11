fdec1afae95791eea5681de35dd5cd99ea6fb545
Onyi13 <njokuonyi562@gmail.com>
2023-09-11 14:30:51 -0700

    AKJFDH

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - Define a new type struct dog with 3 elements
+* @name: First element, name of the dog
+* @age: Second element, age of the dog
+* @owner: Third element, owner of the dog
+*/
+struct dog
+{char *name;
+float age;
+char *owner; };
+#endif /* DOG_H */
