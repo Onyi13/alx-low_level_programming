b6d3aefebecde944c638124be87f0ac755112dae
Onyi13 <njokuonyi562@gmail.com>
2023-09-11 14:36:37 -0700

    SDK

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+* init_dog - Initializes a variable of type struct dog
+* @d: Pointer to the struct dog to initialize
+* @name: The name of the dog
+* @age: The age of the dog
+* @owner: The owner of the dog
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{d->name = name;
+d->age = age;
+d->owner = owner; }}
