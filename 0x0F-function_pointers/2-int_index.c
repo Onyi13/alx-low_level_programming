5e1fcf3f5f6f2780df4cf301bb8db05796ca100b
Onyi13 <njokuonyi562@gmail.com>
2023-09-13 09:21:13 -0700

    SDK

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,12 +7,11 @@
 *Return: index of the first match, -1 if no match or size <= 0
 */
 int int_index(int *array, int size, int (*cmp)(int))
-{
+{int i;
 if (!array || !cmp || size <= 0)
 return (-1);
-for (int i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 if (cmp(array[i]))
-return (i);
-}
+return (i); }
 return (-1); }
