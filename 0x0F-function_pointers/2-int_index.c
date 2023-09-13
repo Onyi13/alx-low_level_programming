66ea96e763dcf2c3ea2c75e44b2bfcde8723339c
Onyi13 <njokuonyi562@gmail.com>
2023-09-13 09:12:33 -0700

    JKSD

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+*int_index - searches for an integer in an array
+*@array: pointer to the array
+*@size: number of elements in the array
+*@cmp: pointer to the comparison function
+*Return: index of the first match, -1 if no match or size <= 0
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+if (!array || !cmp || size <= 0)
+return (-1);
+for (int i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+return (i);
+}
+return (-1); }
