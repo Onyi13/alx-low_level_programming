9bb50347b2ad965987696b0d0ae26a75b15b3819
Onyi13 <njokuonyi562@gmail.com>
2023-09-13 08:28:35 -0700

    SKJ

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_name - prints
+ * a name using the provided function
+ * @name: name to be printed
+ * @f: function to use to print the name
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (!name || !f)
+return;
+f(name); }
