add17fd993d68e26b5ed9df9e7e64d5154c2e820
Onyi13 <njokuonyi562@gmail.com>
2023-09-13 09:24:52 -0700

    DSKFJ

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stddef.h>
 /**
 * array_iterator -
 * executes a function on each array element
@@ -9,8 +8,8 @@
 * Return: Nothing.
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
-{
+{size_t i;
 if (!array || !action)
 return;
-for (size_t i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 action(array[i]); }
