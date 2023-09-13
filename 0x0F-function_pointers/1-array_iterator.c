49b7809f53ad14eb25bd0d35b74ed8a4de6438fa
Onyi13 <njokuonyi562@gmail.com>
2023-09-13 08:44:49 -0700

    FSDKJDS

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,7 @@
 #include "function_pointers.h"
 /**
-* array_iterator - executes a function on each array element
+* array_iterator -
+* executes a function on each array element
 * @array: pointer to the array
 * @size: size of the array
 * @action: function to execute
