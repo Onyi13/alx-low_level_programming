f45ccc3f5ab5fb05b4c97e3919cff2fa737db2ed
Onyi13 <njokuonyi562@gmail.com>
2023-09-21 09:20:12 -0700

    DKJ

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,7 @@
 /**
 * premain - Prints statements before main executes.
 */
+__attribute__((constructor))
 void premain()
 {printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n"); }
