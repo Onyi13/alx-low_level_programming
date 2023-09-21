3df84c429a829d7fd389377a532b1e5a973b747f
Onyi13 <njokuonyi562@gmail.com>
2023-09-21 08:41:31 -0700

    SDKJ

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+* list_len - Returns the number of element
+* in a list_t list.
+* @h: Pointer to the head of the list.
+* Return: Number of nodes.
+*/
+size_t list_len(const list_t *h)
+{size_t nodes = 0;
+while (h != NULL)
+{nodes++;
+h = h->next; }
+return (nodes); }
