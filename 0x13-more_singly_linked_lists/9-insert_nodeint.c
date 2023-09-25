1c8dce54052c64ecc16d3f1a1e1c447f4c2a6ede
Onyi13 <njokuonyi562@gmail.com>
2023-09-25 10:45:53 -0700

    DSFKJ

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,7 +7,6 @@
 *@n: Data for the new node.
 *Return: Address of the new node, or NULL if it fails.
 */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {listint_t *new_node, *temp;
 unsigned int i;
 if (!head)
