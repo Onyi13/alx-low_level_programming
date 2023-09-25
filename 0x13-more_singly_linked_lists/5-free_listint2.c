19aa60d3aed550d1e140cab8fc4a8efa53603ff3
Onyi13 <njokuonyi562@gmail.com>
2023-09-25 10:29:17 -0700

    DFKJS

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+*free_listint2 - Frees a listint_t
+*list and sets head to NULL.
+*@head: Double pointer to the head of the list.
+*/
+void free_listint2(listint_t **head)
+{listint_t *temp;
+if (!head)
+return;
+while (*head)
+{temp = *head;
+*head = (*head)->next;
+free(temp); }
+*head = NULL; }
