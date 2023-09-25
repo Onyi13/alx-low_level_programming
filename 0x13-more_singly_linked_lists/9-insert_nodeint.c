2475f00b06588e128e85c40a6488982df57779c9
Onyi13 <njokuonyi562@gmail.com>
2023-09-25 11:02:12 -0700

    DFKJLS

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,29 +1,33 @@
+#include <stdlib.h>
 #include "lists.h"
 /**
 *insert_nodeint_at_index -
 *Inserts a new node at a given position.
-*@head: Double pointer to the head of the list.
-*@idx: Index at which the new node should be added.
+*@head: Double pointer to the first node.
+*@idx: Index where the new node should be added.
 *@n: Data for the new node.
-*Return: Address of the new node, or NULL if it fails.
+*Return: The address of the new node
+*or NULL if it failed.
 */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {listint_t *new_node, *temp;
-unsigned int i;
-if (!head)
-return (NULL);
+unsigned int i = 0;
 new_node = malloc(sizeof(listint_t));
 if (!new_node)
 return (NULL);
 new_node->n = n;
 new_node->next = NULL;
-if (idx == 0)
-{new_node->next = *head;
-*head = new_node;
+if (!*head && idx == 0)
+{*head = new_node;
 return (new_node); }
+if (!*head && idx > 0)
+{free(new_node);
+return (NULL); }
 temp = *head;
-for (i = 0; i < idx - 1 && temp; i++)
-temp = temp->next;
-if (!temp)
+while (temp && (i < idx - 1))
+{temp = temp->next;
+i++; }
+if (!temp && i < idx - 1)
 {free(new_node);
 return (NULL); }
 new_node->next = temp->next;
