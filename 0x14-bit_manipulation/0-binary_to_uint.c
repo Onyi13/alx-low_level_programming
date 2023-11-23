51da4800a54221f9949145480781ecec22ada992
Onyi13 <njokuonyi562@gmail.com>
2023-11-23 14:07:52 -0800

    DJFSKL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,8 +5,8 @@
  * @b: String containing binary digits
  * Return: The converted number, or 0 if an invalid character is present
  */
-unsigned int binary_to_uint(const char *b) {
-unsigned int result = 0;
+unsigned int binary_to_uint(const char *b)
+{unsigned int result = 0;
 if (b == NULL)
 return (0);
 while (*b != '\0') {
