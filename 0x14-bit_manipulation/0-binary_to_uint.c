13e086bb792f3929d969c0945f71b74806204933
Onyi13 <njokuonyi562@gmail.com>
2023-11-23 13:58:46 -0800

    DFKJ

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int
+ * @b: String containing binary digits
+ * Return: The converted number, or 0 if an invalid character is present
+ */
+unsigned int binary_to_uint(const char *b) {
+unsigned int result = 0;
+if (b == NULL)
+return (0);
+while (*b != '\0') {
+if (*b == '0' || *b == '1')
+{result = result << 1;
+result += (*b - '0'); }
+else
+{
+return (0); }
+b++; }
+return (result); }
